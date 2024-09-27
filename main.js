c7d82dc8e54cc63338dd9d5fd4319da995c3ae4d
lia <140445895+lliawrs@users.noreply.github.com>
2024-09-27 08:51:00 -0300

    ggggg

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ return num1+num2;
 }
 console.log (soma(5,5));
 
-function areaQuadrado (num1,num1){
-    return num1*num1;
+function areaQuadrado (lado){
+    return lado*lado;
 }
 console.log (areaQuadrado(5,5));
 
