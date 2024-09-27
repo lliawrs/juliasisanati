3d1ed9c63ceb44c8ec9dd3352458a86392562660
lia <140445895+lliawrs@users.noreply.github.com>
2024-09-27 09:40:57 -0300

    fffffffffffff

--- main.js
+++ main.js
@@ -11,13 +11,12 @@ console.log (areaQuadrado(5));
 let valoresMedia1 =  parseInt(prompt('insira valor 1'));
 let valoresMedia2 =  parseInt(prompt('insira valor 2'));
 let valoresMedia3 =  parseInt(prompt('insira valor 3'));
-let valoresMedia4 =  parseInt(prompt('insira valor 4'));
 
 let media = function (num1,num2,num3){
     return (num1+num2+num3)/3;
 }
 
-console.log (media(valoresMedia1,valoresMedia2,valoresMedia3:valoresMedia4))
+console.log (media(valoresMedia1,valoresMedia2,valoresMedia3));
 
 
 
