ffa9d0830cd827154e3b9d7f59501bb9932f6917
lia <140445895+lliawrs@users.noreply.github.com>
2024-09-27 09:12:34 -0300

    cccccc

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ function areaQuadrado (lado){
 }
 console.log (areaQuadrado(5));
 
-let media = function (num1,num2,num3,num4){
-    return (num1+num2+num3/num4);
+let media = function (num1,num2,num3){
+    return (num1+num2+num3/3);
 }
 
 
